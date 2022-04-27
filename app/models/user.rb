65b7714903ef2c991ad2b8980059b375bf8dfc0e
kanami55 <summerwave5959@gmail.com>
2022-04-27 21:51:53 +0000

    [Add] user model追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password # password_confirmationカラムが作成される
+
+  # 空でないこと
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true # 一意性制約
 end
