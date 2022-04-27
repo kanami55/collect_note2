959c88ebe34a150e83a7a1740365ae5a54216a26
kanami55 <summerwave5959@gmail.com>
2022-04-27 22:32:07 +0000

    [Add] users show.html.erb 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create, :show, :edit, :update]
   root to: 'homes#top'
   resources :dream_lists
   get '/search_dream_list' => 'dream_lists#search_dream_list' # カテゴリー検索
