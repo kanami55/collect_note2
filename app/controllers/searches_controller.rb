97abc67d41cc946dfdc64279c0449bcb5dfbea7c
kanami55 <summerwave5959@gmail.com>
2022-05-26 10:48:28 +0000

    [Add] search.html.erb

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,5 +2,6 @@ class SearchesController < ApplicationController
   def index
     @dream_lists = DreamList.search(params[:search])
     @search = params[:search]
+    @not_to_do_lists = current_user.not_to_do_lists.all
   end
 end
